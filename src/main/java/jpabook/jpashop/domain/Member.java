e57e4b76c03c396ed424e2ea6eb8c26b6fe6147e
sudong97 <tkddlsqkr21@gmail.com>
2023-08-09 09:10:30 +0900

    [feat] 회원 테이블 생성jpa 작성

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 이걸 사용하는 순간, 나는 order테이블에 있는 member의 거울일 뿐임을 선언하는 것임.
+    private List<Order> orders = new ArrayList<>();
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+// 상속관계이기 때문에 전략을 잡아줘야 한다.
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<Category>();
+
+
+}
